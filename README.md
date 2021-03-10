951d3b9c92944e70d171b3a34df11de37d909307
Brian Seidl <brian.seidl@coxautoinc.com>
2021-03-10 14:56:34 -0500

    remove terraform

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # scrumnest-api
+
+# TODO
--- terraform/infrastructure/outputs.tf
+++
@@ -1,7 +0,0 @@
-output "dynamodb-table-name" {
-  value = aws_dynamodb_table.scrumnest-dynamodb-table.name
-}
-
-output "dynamodb-table-arn" {
-  value = aws_dynamodb_table.scrumnest-dynamodb-table.arn
-}
